d45de7f16cff0ed9adccf38c63bde1c30ad5e508
Steve Zhuo <wqzhuo@gmail.com>
2019-04-21 00:16:39 -0400

    template

--- headlines.py
+++ headlines.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 import feedparser
 
 app = Flask(__name__)
@@ -17,16 +18,7 @@ def cnn():
 
 def get_news(publication):
     feed = feedparser.parse(RSS_FEEDS[publication])
-    first_article = feed['entries'][0]
-    return """<html>
-       <body>
-           <h1>Headlines </h1>
-           <b>{0}</b> </ br>
-           <i>{1}</i> </ br>
-           <p>{2}</p> </ br>
-       </body>
-   </html>""".format(first_article.get("title"), first_article.
-   get("published"), first_article.get("summary"))
+    return render_template("home.html", articles=feed['entries'])
 
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
